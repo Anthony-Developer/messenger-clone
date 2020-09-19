0ddf2497df7be849cd3c4bae669dd2d86053ed97
Anthony Jimenez <anthonyjimenez@anthonys-mbp-2.myfiosgateway.com>
2020-09-19 13:16:53 -0400

    Added structure to username and text

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
+import {Button, FormControl, InputLabel, Input} from '@material-ui/core'
 import './App.css'
+import Messages from './Components/Messages'
 
 function App() {
   const [input, setInput] = useState('')
-  const [messages, setMessages] = useState(['test1', 'test2', 'test3'])
+  const [messages, setMessages] = useState([{username: 'Steve', text: 'Hello World'}, {username: 'Mike', text: 'What World?'}, {username: 'John', text: 'This one Dummy!'}])
+  const [username, setUsername] = useState('')
+
+  useEffect(() => {
+    setUsername(prompt('Please enter your Username'))
+  }, [])
 
   // When you hit send messages it appends your text to the array of messages
   const sendMessage = (e) => {
-    setMessages([...messages, input])
+    e.preventDefault()
+    setMessages([...messages, {username: username, text: input}])
     setInput('')
   }
-
-  console.log(messages)
 
   return (
 
     <div className="App">
 
       <h1>Facebook Messenger</h1>
+      <h3>Welcome {username}</h3>
 
-      <input value={input} onChange={e => setInput(e.target.value)}/>
-      <button onClick={sendMessage}> Send </button>
+      {messages.map(message => (
+        <Messages username={message.username} text={message.text}/>
+      ))}
+
+      <form>
+
+      <FormControl>
+        <InputLabel >Enter Message </InputLabel>
+        <Input value={input} onChange={e => setInput(e.target.value)}/>
+        <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}> Send </Button>
+      </FormControl>
+
+      </form>
+      
 
     </div>
 
---
+++ src/Components/Messages.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Messages(props) {
+
+    return (
+        <div>
+            <h2>{props.username} : {props.text}</h2>
+        </div>
+    )
+}
+
+export default Messages
