e5746847d5335b6dfff8025e701e32b77d57fef5
Anthony Jimenez <anthonyjimenez@anthonys-mbp-2.myfiosgateway.com>
2020-09-19 11:59:52 -0400

    Added a function to append your input to the array of messages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [input, setInput] = useState('')
+  const [messages, setMessages] = useState(['test1', 'test2', 'test3'])
+
+  // When you hit send messages it appends your text to the array of messages
+  const sendMessage = (e) => {
+    setMessages([...messages, input])
+    setInput('')
+  }
+
+  console.log(messages)
+
   return (
+
     <div className="App">
-      <h1>Hello Messenger</h1>
+
+      <h1>Facebook Messenger</h1>
+
+      <input value={input} onChange={e => setInput(e.target.value)}/>
+      <button onClick={sendMessage}> Send </button>
+
     </div>
+
   )
 }
 
